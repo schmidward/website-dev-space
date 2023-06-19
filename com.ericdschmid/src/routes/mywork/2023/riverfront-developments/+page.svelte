bea42bb9d2067aa3bd307da6bf97b9d717d24a96
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-06-18 18:12:15 -0600

    working on riverfront-developments page slash tempalte page for locally hosted published articles.

--- com.ericdschmid/src/routes/mywork/2023/riverfront-developments/+page.svelte
+++ com.ericdschmid/src/routes/mywork/2023/riverfront-developments/+page.svelte
@@ -2,10 +2,48 @@
 
 </script>
 
-<body>
-    <p>Hello world!</p>
-</body>
+<section>
+    <div class="main-content-wrapper">
+        <div class="hed">
+            <h1>Riverfront developments face obstacles, but the Mississippi River itself may be the biggest</h1>
+        </div>
+        <div class="img-and-caption">
+            <img src="../../src/lib/images/2023-02-06_BM_RIVER-26.jpeg" alt="The Mississippi River"/>
+            <p>The Old Chain of Rocks Bridge is seen in the distance last week from the middle of the Mississippi River. 
+                Over the years, developers have been challenged in trying to tame the mighty river's shores for the sake 
+                of economic development. CREDIT: <em>BRIAN MUÑOZ / ST. LOUIS PUBLIC RADIO</em></p>
+        </div>
+        <article>
+            <p class="pub">This article was originally <a href="https://news.stlpublicradio.org/economy-business/2023-02-14/riverfront-developments-face-obstacles-but-the-mississippi-river-itself-may-be-the-biggest">published by St. Louis Public Radio</a> on February 14th, 2023.</p>
+            
+        </article>
+    </div>
+</section> 
 
 <style>
+.main-content-wrapper {
+    max-width: 992px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
 
+.hed h1 {
+    font-size: 2.25em;
+    margin-top: 1%;
+    margin-bottom: 1%;
+}
+
+.pub {
+    text-align: center;
+}
+
+.img-and-caption p {
+    margin: 0;
+    font-size: 12px;
+}
+
+img {
+    width: 100%;
+}
 </style>

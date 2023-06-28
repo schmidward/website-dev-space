b0dabfa4842740fcc6714fc3996bd1b96a18e735
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-06-28 16:36:07 -0500

    Home page updates. Image styling is killing me slowly

--- com.ericdschmid/src/routes/+page.svelte
+++ com.ericdschmid/src/routes/+page.svelte
@@ -9,70 +9,31 @@
         style="background-image: url('./src/lib/images/home-image-bikes.jpeg');">
         <div class="bg-blue/40 w-full h-full">
             <div class="flex flex-col items-center justify-center w-full h-full">
-                <p class="text-white font-semibold text-5xl sm:text-7xl md:text-9xl">Eric D. Schmid</p>
-                <p class="text-white text-3xl sm:text-5xl md:text-7xl mt-7">Investigative Journalist | Software Engineer</p>
+                <p class="text-white font-semibold text-5xl sm:text-7xl md:text-9xl text-center">Eric D. Schmid</p>
+                <p class="text-white text-3xl sm:text-5xl md:text-7xl mt-7 text-center">Investigative Journalist | Software Engineer</p>
             </div>
         </div>
     </div>
 </section>
 <section>
-    <div class="w-full h-full">
-        <div class="flex flex-row justify-center mt-10">
-            <img class="w-1/4 mr-5" src="./src/lib/images/2022-02-11_ES_Alicia-Steele.jpeg" alt="Alicia Steel sitting with her son's service dog."/>
-            <img class="w-1/4 ml-5 mr-5" src="./src/lib/images/ranch-sunset.jpeg" alt="A sunset in Wyoming."/>
-        </div>
-        <div class="flex flex-row justify-center mt-10">
-            <img class="w-1/4 mr-5" src="./src/lib/images/2022-02-11_ES_Alicia-Steele.jpeg" alt="Alicia Steel sitting with her son's service dog."/>
-            <img class="w-1/4 ml-5 mr-5" src="./src/lib/images/2022-02-11_ES_Alicia-Steele.jpeg" alt="Alicia Steel sitting with her son's service dog."/>
-        </div>
-
-
+    <div class="m-16 md:mr-60 md:ml-60 md:mt-20 md:mb-20 grid gap-4 grid-cols-2">
+        <img class="object-cover" src="./src/lib/images/2022-02-11_ES_Alicia-Steele.jpeg" alt="Alicia Steel sitting with her son's service dog."/>    
+        <img class="object-cover h-full" src="./src/lib/images/ranch-sunset.jpeg" alt="A sunset in Wyoming."/>
+        <img class="object-cover h-100" src="./src/lib/images/2022-08-14_ES_Underground-Cave.jpeg" alt="Alicia Steel sitting with her son's service dog."/>
+        <img class="object-cover object-center h-full" src="./src/lib/images/nascar.jpeg" alt="Alicia Steel sitting with her son's service dog."/>
     </div>
 
-<article class="content__inner">
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-    <p>Some text</p>
-</article>
+<div>
+    Eric Schmid is a multimedia journalist based in St. Louis with 5+ years experience primarily in print-, digital-, and audio-focused newsrooms. He has demonstrated success developing strong continuing news coverage and producing large scale reporting projects independently and as part of a team. This includes advanced skill with source building, story pitching, public records, long-term coverage planning, interviewing, and audio editing/mixing. 
+
+His reporting experience spans from breaking hard news to enterprise and investigative features and he excels at producing content native to digital/print and audio/radio platforms. Eric gravitates toward complicated stories with many moving parts and enjoys the challenge of ingesting and distilling large swaths of information into cohesive, intriguing, and manageable stories. He is adept at covering most topics, though his enterprise work regularly returns to stories about the environment, public policy, and the census and demographics.
+
+Eric joined St. Louis Public Radio in June 2019 as their first ever Metro East Reporter, tasked with expanding and developing the station’s coverage of suburban St. Louis communities east of the Mississippi River. In June 2022, he was promoted to cover economic development across St. Louis, a region of 2.8 million people. 
+
+Before joining St. Louis Public Radio, Eric held internships at Fox News Channel, NPR member station WSHU Public Radio, and AccuWeather. He graduated from Stony Brook University in 2018 as the valedictorian of the School of Journalism.
+
+Lead image by Brian Muñoz. All other photography on this page by Eric Schmid.
+</div>
 
 </section> 
 </body>

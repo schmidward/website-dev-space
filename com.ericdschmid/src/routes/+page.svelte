095a0d8ca7c58b7bc1614745677d5e0b2dd2773b
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-06-28 16:48:32 -0500

    cropped some images to make my life easier

--- com.ericdschmid/src/routes/+page.svelte
+++ com.ericdschmid/src/routes/+page.svelte
@@ -23,16 +23,21 @@
         <img class="object-cover object-center h-full" src="./src/lib/images/nascar.jpeg" alt="Alicia Steel sitting with her son's service dog."/>
     </div>
 
-<div>
-    Eric Schmid is a multimedia journalist based in St. Louis with 5+ years experience primarily in print-, digital-, and audio-focused newsrooms. He has demonstrated success developing strong continuing news coverage and producing large scale reporting projects independently and as part of a team. This includes advanced skill with source building, story pitching, public records, long-term coverage planning, interviewing, and audio editing/mixing. 
-
-His reporting experience spans from breaking hard news to enterprise and investigative features and he excels at producing content native to digital/print and audio/radio platforms. Eric gravitates toward complicated stories with many moving parts and enjoys the challenge of ingesting and distilling large swaths of information into cohesive, intriguing, and manageable stories. He is adept at covering most topics, though his enterprise work regularly returns to stories about the environment, public policy, and the census and demographics.
-
-Eric joined St. Louis Public Radio in June 2019 as their first ever Metro East Reporter, tasked with expanding and developing the station’s coverage of suburban St. Louis communities east of the Mississippi River. In June 2022, he was promoted to cover economic development across St. Louis, a region of 2.8 million people. 
-
-Before joining St. Louis Public Radio, Eric held internships at Fox News Channel, NPR member station WSHU Public Radio, and AccuWeather. He graduated from Stony Brook University in 2018 as the valedictorian of the School of Journalism.
-
-Lead image by Brian Muñoz. All other photography on this page by Eric Schmid.
+<div class="max-w-screen-lg block mr-auto ml-auto text-2xl">
+    <p class="m-7">Eric Schmid is a multimedia journalist based in St. Louis with 5+ years experience primarily in print-, digital-, and audio-focused newsrooms. 
+        He has demonstrated success developing strong continuing news coverage and producing large scale reporting projects independently and as part 
+        of a team. This includes advanced skill with source building, story pitching, public records, long-term coverage planning, interviewing, and 
+        audio editing/mixing.</p>
+    <p class="m-7">His reporting experience spans from breaking hard news to enterprise and investigative features and he excels at producing content native to 
+        digital/print and audio/radio platforms. Eric gravitates toward complicated stories with many moving parts and enjoys the challenge of ingesting 
+        and distilling large swaths of information into cohesive, intriguing, and manageable stories. He is adept at covering most topics, though his 
+        enterprise work regularly returns to stories about the environment, public policy, and the census and demographics.</p>
+    <p class="m-7">Eric joined St. Louis Public Radio in June 2019 as their first ever Metro East Reporter, tasked with expanding and developing the station’s 
+        coverage of suburban St. Louis communities east of the Mississippi River. In June 2022, he was promoted to cover economic development across St. 
+        Louis, a region of 2.8 million people.</p>
+    <p class="m-7">Before joining St. Louis Public Radio, Eric held internships at Fox News Channel, NPR member station WSHU Public Radio, and AccuWeather. He graduated 
+        from Stony Brook University in 2018 as the valedictorian of the School of Journalism.</p>
+    <p class="m-7">Lead image by Brian Muñoz. All other photography on this page by Eric Schmid.</p>
 </div>
 
 </section> 

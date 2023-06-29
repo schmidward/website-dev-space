a267306be50a3c6d03c787cc36f1011ea38af7bf
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-06-29 17:41:42 -0500

    adding resume route and parts of the about me page

--- com.ericdschmid/src/routes/about/+page.svelte
+++ com.ericdschmid/src/routes/about/+page.svelte
@@ -1,4 +1,15 @@
 
-<body>
-    
-</body>
+<section class="relative w-screen h-screen">
+    <div 
+        class="bg-fixed bg-left-top bg-cover w-full h-1/2" 
+        style="background-image: url('./src/lib/images/night-sky.jpeg');">
+        <div class="bg-blue/20 w-full h-full">
+            <div class="flex flex-col items-center justify-center w-full h-full">
+                <p class="text-white font-semibold text-5xl sm:text-7xl md:text-9xl text-center">About Me</p>
+            </div>
+        </div>
+    </div>
+    <div>
+        
+    </div>
+</section>

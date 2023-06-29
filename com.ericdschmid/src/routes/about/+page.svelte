00d7bc631c1b321626aa627a72b7b1081e41badb
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-06-29 17:43:49 -0500

    final commit of the night

--- com.ericdschmid/src/routes/about/+page.svelte
+++ com.ericdschmid/src/routes/about/+page.svelte
@@ -1,9 +1,9 @@
 
 <section class="relative w-screen h-screen">
     <div 
-        class="bg-fixed bg-left-top bg-cover w-full h-1/2" 
+        class="bg-fixed bg-center bg-cover w-full h-1/2" 
         style="background-image: url('./src/lib/images/night-sky.jpeg');">
-        <div class="bg-blue/20 w-full h-full">
+        <div class="bg-blue/40 w-full h-full">
             <div class="flex flex-col items-center justify-center w-full h-full">
                 <p class="text-white font-semibold text-5xl sm:text-7xl md:text-9xl text-center">About Me</p>
             </div>

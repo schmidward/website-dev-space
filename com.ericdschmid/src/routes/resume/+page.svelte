c2068cac5371ada6d18076ac5a249c70dbe87e1d
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-06-30 13:08:36 -0500

    halfway through hardcoding resume... thinking of switching to dynamic generation with JSON

--- com.ericdschmid/src/routes/resume/+page.svelte
+++ com.ericdschmid/src/routes/resume/+page.svelte
@@ -4,5 +4,40 @@
             Resume
         </div>
         <hr />
+            <div class="mt-8 flex flex-col">
+                <div>
+                    <p class="-ml-1 text-8xl font-bold">Eric D. Schmid</p>
+                    <p class="text-2xl font-semibold ">Software Developer & Investigative Reproter in St. Louis MO</p>
+                    <p class="font-light text-lg mt-2"> I excel asking thoughtful and pointed questions that cut to the heart of an issue or topic and 
+                        I relish the chllenge of distilling complex information into clear and manageable packages. My 
+                        experience as an investigative journalist equips me with the skills to quickly solve complex 
+                        problems and it means I'm comfortable communicating and collaborating with anyone.</p>
+                    <p class="font-extralight text-lg  mt-3">
+                        GitHub: <span class="text-dark-red hover:text-hover-red"><a href="https:/github.com/schmidward" target="_blank">https:/github.com/schmidward</a></span> | 
+                        LinkedIn: <span class="text-dark-red hover:text-hover-red"><a href="https:/www.linkedin.com/in/ericdschmid/" target="_blank">https:/www.linkedin.com/in/ericdschmid/</a></span>
+                    </p>
+                    <p class="text-2xl font-semibold mt-3 ">Project Experience</p>
+                    <p class="text-lg"><span class="font-semibold ">User Administration Context</span> — Web based user registration and login service</p>
+                    <p class="-mt-1 text-base">• Implements Spring Security, with JWT token, CSRF cookies, REST and remote database to administer users and secure private APIs</p>
+                    <p class="-mt-1 text-base">• Frontend user context, login and registration designed in React</p>
+                    
+                    <p class="mt-3 text-lg"><span class="font-semibold ">www.ericdschmid.com</span> — Redesign of personal portfolio website to leverage Svelte and Tailwindcss frameworks</p>
+                    <p class="-mt-1 text-base">• Features custom built components and routing to display static news content dynamically across all screen sizes</p>
+                    <p class="-mt-1 text-base">• Implements JSON and OOP principles for lightweight, scaleable, and reusable code</p>
+                </div>
+                <div class="flex flex-row">
+                    <div class="w-4/5">
+                        <p class="text-2xl font-semibold mt-3 ">Professional Experience</p>
+                        <p class="text-lg"><span class="font-semibold ">Economic Development Reporter</span> — St. Louis Public Radio, St. Louis, MO</p>
+                        <p class="-mt-1 font-light"><em>June 2022 - Present</em></p>
+                        <p class="">Researches and writes stories on efforts from businesses’, governments’, nonprofits’, and others to create economic opportunity and grow the population in Greater St. Louis</p>
+                        <p class="-mt-1 ml-5 text-base">• Promoted after three years of outstanding performance in previous position</p>
+                        <p class="-mt-1 ml-5 text-base">• Facilitates and leads interviews with people from many backgrounds including elected officials, business executives and military leaders</p>
+                    </div>
+                    <div class="w-1/5">
+                        <p class="text-2xl font-semibold mt-3 ">Skills</p>
+                    </div>
+                </div>
+            </div>
     </div>
 </section>

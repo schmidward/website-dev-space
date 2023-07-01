83507de2cd94f45bf2e1226be1fdcc95b276bcbd
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-07-01 18:13:12 -0500

    starting to implement JSON for the resume page.

--- com.ericdschmid/src/routes/resume/+page.svelte
+++ com.ericdschmid/src/routes/resume/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import resumes from "../../lib/content/resume.json"
+    let resume = resumes.software;
+    console.log(resume);
+</script>
+
 <section>
     <div class="max-w-screen-lg block mr-auto ml-auto">
         <div class="m-10 text-7xl font-semibold text-center">
@@ -14,24 +20,21 @@
         <div class="ml-8 mr-8 mb-10 flex flex-col">
             <hr class="mb-8" />
                 <div>
-                    <p class="-ml-1 text-8xl font-bold">Eric D. Schmid</p>
-                    <p class="text-2xl font-semibold ">Software Developer in St. Louis MO</p>
-                    <p class="font-light text-lg mt-2"> I excel asking thoughtful and pointed questions that cut to the heart of an issue or topic and 
-                        I relish the chllenge of distilling complex information into clear and manageable packages. My 
-                        experience as an investigative journalist equips me with the skills to quickly solve complex 
-                        problems and it means I'm comfortable communicating and collaborating with anyone.</p>
+                    <p class="-ml-1 text-8xl font-bold">{resume.name}</p>
+                    <p class="text-2xl font-semibold ">{resume.subhead}</p>
+                    <p class="font-light text-lg mt-2">{resume.dek}</p>
                     <p class="font-extralight text-lg  mt-3">
-                        GitHub: <span class="text-dark-red hover:text-hover-red"><a href="https:/github.com/schmidward" target="_blank">https:/github.com/schmidward</a></span> | 
-                        LinkedIn: <span class="text-dark-red hover:text-hover-red"><a href="https:/www.linkedin.com/in/ericdschmid/" target="_blank">https:/www.linkedin.com/in/ericdschmid/</a></span>
+                        {#each resume.links as link}
+                        {link.text}<span class="text-dark-red hover:text-hover-red"><a href="{link.href}" target="_blank">{link.href}</a></span>
+                        {/each}
                     </p>
-                    <p class="text-2xl font-semibold mt-3 ">Project Experience</p>
-                    <p class="text-lg"><span class="font-semibold ">User Administration Context</span> — Web based user registration and login service</p>
-                    <p class="leading-tight text-base">• Implements Spring Security, with JWT token, CSRF cookies, REST and remote database to administer users and secure private APIs</p>
-                    <p class="leading-tight text-base">• Frontend user context, login and registration designed in React</p>
-                    
-                    <p class="mt-3 text-lg"><span class="font-semibold ">www.ericdschmid.com</span> — Redesign of personal portfolio website to leverage Svelte and Tailwindcss frameworks</p>
-                    <p class="leading-tight text-base">• Features custom built components and routing to display static news content dynamically across all screen sizes</p>
-                    <p class="leading-tight text-base">• Implements JSON and OOP principles for lightweight, scaleable, and reusable code</p>
+                    <p class="text-2xl font-semibold mt-3 ">{resume.projectHed}</p>
+                    {#each resume.projects as project}
+                    <p class="text-lg mt-2"><span class="font-semibold">{project.title}</span> — {project.subhead}</p>
+                        {#each project.dek as dek}
+                    <p class="leading-tight text-base">{dek}</p>
+                        {/each}
+                    {/each}
                 </div>
                 <div class="flex flex-col lg:flex-row">
                     <div class="lg:w-9/12">

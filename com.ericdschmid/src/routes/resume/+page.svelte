58e654a7a471bb274d662683cf4210cbda5267c4
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-07-01 17:25:52 -0500

    Resume inputted. Now to turn it into JSON and use if blocks and for each to display similar to mywork

--- com.ericdschmid/src/routes/resume/+page.svelte
+++ com.ericdschmid/src/routes/resume/+page.svelte
@@ -2,20 +2,20 @@
     <div class="max-w-screen-lg block mr-auto ml-auto">
         <div class="m-10 text-7xl font-semibold text-center">
             Resume
-            <div class="flex flex-col ml-44 mr-44 mt-3 mb-4 md:ml-0 md:mr-0 md:flex-row md:justify-around">
+            <div class="flex flex-col ml-44 mr-44 mt-6 mb-4 text-xl md:ml-0 md:mr-0 md:flex-row md:justify-around">
                 <div class="bg-red text-white mt-3 mb-3 p-5 text-center hover:bg-hover-red" >
                     Journalism
                 </div>
                 <div class="bg-red text-white mt-3 mb-3 p-5 text-center hover:bg-hover-red" >
                     Software Development
                 </div>
-                </div>
+            </div>
         </div>
-        <hr />
-            <div class="mt-8 flex flex-col">
+        <div class="ml-8 mr-8 flex flex-col">
+            <hr class="mb-8" />
                 <div>
                     <p class="-ml-1 text-8xl font-bold">Eric D. Schmid</p>
-                    <p class="text-2xl font-semibold ">Software Developer & Investigative Reproter in St. Louis MO</p>
+                    <p class="text-2xl font-semibold ">Software Developer in St. Louis MO</p>
                     <p class="font-light text-lg mt-2"> I excel asking thoughtful and pointed questions that cut to the heart of an issue or topic and 
                         I relish the chllenge of distilling complex information into clear and manageable packages. My 
                         experience as an investigative journalist equips me with the skills to quickly solve complex 
@@ -26,24 +26,91 @@
                     </p>
                     <p class="text-2xl font-semibold mt-3 ">Project Experience</p>
                     <p class="text-lg"><span class="font-semibold ">User Administration Context</span> — Web based user registration and login service</p>
-                    <p class="-mt-1 text-base">• Implements Spring Security, with JWT token, CSRF cookies, REST and remote database to administer users and secure private APIs</p>
-                    <p class="-mt-1 text-base">• Frontend user context, login and registration designed in React</p>
+                    <p class="leading-tight text-base">• Implements Spring Security, with JWT token, CSRF cookies, REST and remote database to administer users and secure private APIs</p>
+                    <p class="leading-tight text-base">• Frontend user context, login and registration designed in React</p>
                     
                     <p class="mt-3 text-lg"><span class="font-semibold ">www.ericdschmid.com</span> — Redesign of personal portfolio website to leverage Svelte and Tailwindcss frameworks</p>
-                    <p class="-mt-1 text-base">• Features custom built components and routing to display static news content dynamically across all screen sizes</p>
-                    <p class="-mt-1 text-base">• Implements JSON and OOP principles for lightweight, scaleable, and reusable code</p>
+                    <p class="leading-tight text-base">• Features custom built components and routing to display static news content dynamically across all screen sizes</p>
+                    <p class="leading-tight text-base">• Implements JSON and OOP principles for lightweight, scaleable, and reusable code</p>
                 </div>
                 <div class="flex flex-row">
-                    <div class="w-4/5">
+                    <div class="w-9/12">
                         <p class="text-2xl font-semibold mt-3 ">Professional Experience</p>
-                        <p class="text-lg"><span class="font-semibold ">Economic Development Reporter</span> — St. Louis Public Radio, St. Louis, MO</p>
-                        <p class="-mt-1 font-light"><em>June 2022 - Present</em></p>
-                        <p class="">Researches and writes stories on efforts from businesses’, governments’, nonprofits’, and others to create economic opportunity and grow the population in Greater St. Louis</p>
-                        <p class="-mt-1 ml-5 text-base">• Promoted after three years of outstanding performance in previous position</p>
-                        <p class="-mt-1 ml-5 text-base">• Facilitates and leads interviews with people from many backgrounds including elected officials, business executives and military leaders</p>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">Economic Development Reporter</span> — St. Louis Public Radio, St. Louis, MO</p>
+                            <p class="-mt-1 font-light"><em>June 2022 - Present</em></p>
+                            <p class="leading-none">Researches and writes stories on efforts from businesses’, governments’, nonprofits’, and others to create economic opportunity and grow the population in Greater St. Louis</p>
+                            <p class="mt-1 ml-5 leading-tight text-base">• Promoted after three years of outstanding performance in previous position</p>
+                            <p class="ml-5 leading-tight text-base">• Facilitates and leads interviews with people from many backgrounds including elected officials, business executives and military leaders</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">Metro East Reporter</span> — St. Louis Public Radio, Belleville, IL</p>
+                            <p class="-mt-1 font-light"><em>June 2019 - June 2022</em></p>
+                            <p class="leading-none">Worked remotely producing articles about Illinois communities adjacent to St. Louis</p>
+                            <p class="mt-1 ml-5 leading-tight text-base">• Pioneered news coverage for 750,000 people who hadn’t been previously served</p>
+                            <p class="ml-5 leading-tight text-base">• Produced web content reaching more than 200,000 unique users annually</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">Freelance Reporter</span> — National Public Radio, Marketplace, Science Friday</p>
+                            <p class="-mt-1 font-light"><em>April 2018 - Present</em></p>
+                            <p class="leading-none">Pitches, reports, and produces original news content for national platforms</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">Dow Jones News Fund Digital Media Intern</span> — AccuWeather, State College, PA</p>
+                            <p class="-mt-1 font-light"><em>June 2018 - August 2018</em></p>
+                            <p class="leading-none">Produced video content toping five million views on company website and socials</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">College Associate</span> — Shepard Smith Reporting, Fox News Channel, New York, NY</p>
+                            <p class="-mt-1 font-light"><em>June 2017 - August 2017</em></p>
+                            <p class="leading-none">Sourced video content for a live national broadcast reaching millions of people each day</p>
+                        </div>
+                        <p class="text-2xl font-semibold mt-3 ">Education</p>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">LaunchCode</span> - St. Louis, MO — September 2022 - June 2023</p>
+                            <p class="leading-none">30-week full-stack development course in JavaScript, Angular, Java, and Spring Boot</p>
+                            <p class="mt-1 ml-5 leading-tight text-base">• Project experience reviewing and completing code bases to unit test specifications</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg"><span class="font-semibold">Stony Brook University</span> - Stony Brook, NY — Class of 2018</p>
+                            <p class="leading-none">Bachelor of Arts in Journalism, Business Management Minor</p>
+                        </div>
                     </div>
-                    <div class="w-1/5">
+                    <div class="w-3/12">
                         <p class="text-2xl font-semibold mt-3 ">Skills</p>
+                        <div class="mb-2">
+                            <p class="text-lg font-semibold">Programming Languages</p>
+                            <p class="leading-tight text-lg">• Java</p>
+                            <p class="leading-tight text-lg">• JavaScript / TypeScript</p>
+                            <p class="leading-tight text-lg">• HTML5 / CSS3</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg font-semibold">Frameworks</p>
+                            <p class="leading-tight text-lg">• React</p>
+                            <p class="leading-tight text-lg">• Svelte</p>
+                            <p class="leading-tight text-lg">• Spring Boot</p>
+                            <p class="leading-tight text-lg">• Spring Security</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg font-semibold">Databases</p>
+                            <p class="leading-tight text-lg">• MySQL</p>
+                            <p class="leading-tight text-lg">• PostgreSQL</p>
+                        </div>
+                        <div class="mb-2">
+                            <p class="text-lg font-semibold">Tools</p>
+                            <p class="leading-tight text-lg">• Grunt</p>
+                            <p class="leading-tight text-lg">• Node</p>
+                            <p class="leading-tight text-lg">• REST</p>
+                        </div>
+                        <p class="text-2xl font-semibold mt-3 ">Awards & Honors</p>
+                        <div class="mt-2 mb-3">
+                            <p class="text-lg font-semibold leading-none">2022 IRE Award Finalist & 2023 Region 5 Murrow Award</p>
+                            <p class="leading-none mt-1">For original and revelatory watchdog journalism exposing a 25-year coverup of toxic groundwater contamination in Springfield, Missouri.</p>
+                        </div>
+                        <div class="mt-2 mb-2">
+                            <p class="text-lg font-semibold leading-none">Keynote Speaker</p>
+                            <p class="leading-none mt-1">2022 Mississippi River Basin Ag & Water Desk Conference</p>
+                        </div>
                     </div>
                 </div>
             </div>

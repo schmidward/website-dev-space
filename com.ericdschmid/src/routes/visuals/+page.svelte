1d036415ff4b8c95db5849ee5522672f7dac05e3
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-07-10 17:16:18 -0500

    D3 is mercifully working now

--- com.ericdschmid/src/routes/visuals/+page.svelte
+++ com.ericdschmid/src/routes/visuals/+page.svelte
@@ -1,18 +1,6 @@
 <script>
-    import * as d3 from "d3";
-
-    export let data;
-    export let width = 640;
-    export let height = 400;
-    export let marginTop = 20;
-    export let marginRight = 20;
-    export let marginBottom = 20;
-    export let marginLeft = 20;
-
-    $: x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
-    $: y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
-    $: line = d3.line((d, i) => x(i), y);
-
+  import data from "./2019.js";
+  import BarChart from "./BarChart.svelte";
 </script>
 
 <section>
@@ -20,14 +8,5 @@
         Visuals
     </div>
 
-    <svg width={width} height={height}>
-        <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
-        <g fill="white" stroke="currentColor" stroke-width="1.5">
-          {#each data as d, i}
-            <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
-          {/each}
-        </g>
-      </svg>
-
-
+    <BarChart {data} />
 </section>
